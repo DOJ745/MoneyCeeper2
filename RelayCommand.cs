1e4da7a90c00c10d91dc8b886edb2965f5a749e7
DOJ745 <andreyfa12@mail.ru>
2020-04-23 19:40:40 +0300

    Обновил RelayCommand

--- RelayCommand.cs
+++ RelayCommand.cs
@@ -5,40 +5,34 @@ namespace MoneyCeeper
 {
     class RelayCommand : ICommand
     {
-        private Action<object> execute;
-        private Func<object, bool> canExecute;
-
-        /// <summary>
-        /// Событие извещающее об измении состояния команды
-        /// </summary>
-        public event EventHandler CanExecuteChanged
-        {
-            add { CommandManager.RequerySuggested += value; }
-            remove { CommandManager.RequerySuggested -= value; }
-        }
-
+        private readonly Func<object, bool> _canExecute;
+        private readonly Action<object> _onExecute;
+        private readonly EventHandler _requerySuggested;
+ 
+        /// <summary>Событие извещающее об измении состояния команды</summary>
+        public event EventHandler CanExecuteChanged;
+ 
         /// <summary>Конструктор команды</summary>
         /// <param name="execute">Выполняемый метод команды</param>
         /// <param name="canExecute">Метод разрешающий выполнение команды</param>
         public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
         {
-            this.execute = execute;
-            this.canExecute = canExecute;
+            _onExecute = execute;
+            _canExecute = canExecute;
+ 
+            _requerySuggested = (o, e) => Invalidate();
+            CommandManager.RequerySuggested += _requerySuggested;
         }
-
+ 
+        public void Invalidate() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+ 
         /// <summary>Вызов разрешающего метода команды</summary>
         /// <param name="parameter">Параметр команды</param>
         /// <returns>True - если выполнение команды разрешено</returns>
-        public bool CanExecute(object parameter)
-        {
-            return this.canExecute == null || this.canExecute(parameter);
-        }
-
+        public bool CanExecute(object parameter) => _canExecute == null ? true : _canExecute.Invoke(parameter);
+ 
         /// <summary>Вызов выполняющего метода команды</summary>
         /// <param name="parameter">Параметр команды</param>
-        public void Execute(object parameter)
-        {
-            this.execute(parameter);
-        }
+        public void Execute(object parameter) => _onExecute?.Invoke(parameter);
     }
 }
