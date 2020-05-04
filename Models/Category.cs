01ccbe08026b2014cd64cbe6ca1525eaf7d3ff01
DOJ745 <andreyfa12@mail.ru>
2020-05-04 14:25:47 +0300

    Перечисление категорий
    Добавил категории

---
+++ Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyCeeper.Models
+{
+    public enum Categoty
+    {
+        Food, Cloth, Prezents, Kids, 
+        Care, Heatlh, Cafe, Education,
+        Entertainment, Transport, Commission
+
+        // Care - забота о себе
+        // Entertainment - развлечения различного рода
+    }
+}
