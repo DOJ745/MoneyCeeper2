e4e3888cef808a11e3424000a29da8e5c9ae3330
DOJ745 <andreyfa12@mail.ru>
2020-05-07 17:21:01 +0300

    Грамотный контекст

--- Models/CostContext.cs
+++ Models/CostContext.cs
@@ -11,6 +11,7 @@ namespace MoneyCeeper
         }
 
         public virtual DbSet<Cost> Costs { get; set; }
+        public virtual DbSet<User> Users { get; set; }
 
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
