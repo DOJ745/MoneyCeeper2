19bd4962b36bb6efface48d7245b35f0808f99ba
DOJ745 <andreyfa12@mail.ru>
2020-05-07 17:26:07 +0300

    Update Cost.cs

--- Models/Cost.cs
+++ Models/Cost.cs
@@ -9,8 +9,6 @@ namespace MoneyCeeper
         #region Fields
         public int Cost_Id { get; set; }
         public double Price { get; set; }
-
-        //public string User_Name { get; set; }
         public string Comment { get; set; }
         public string Description { get; set; }
         public DateTime Date_Time { get; set; }
