bc26805d8973cde7a842c3665173f718a2ad27a9
DOJ745 <andreyfa12@mail.ru>
2020-05-08 09:41:28 +0300

    Добавил атрибуты

--- Models/Cost.cs
+++ Models/Cost.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Runtime.CompilerServices;
 
 namespace MoneyCeeper
@@ -7,11 +8,21 @@ namespace MoneyCeeper
     class Cost : ViewModelBase
     {
         #region Fields
+        [Key]
         public int Cost_Id { get; set; }
+
+        [Required]
         public double Price { get; set; }
+
+        [Required]
+        [MaxLength(300)]
         public string Comment { get; set; }
+
+        [MaxLength(500)]
         public string Description { get; set; }
         public DateTime? Date_Time { get; set; }
+
+        [Required]
         public Category Category_Type { get; set; }
 
         public string Username { get; set; }
--- Models/User.cs
+++ Models/User.cs
@@ -1,14 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Runtime.CompilerServices;
 
 namespace MoneyCeeper
 {
     class User : ViewModelBase
     {
+        [Key]
+        [MaxLength(32)]
         public string Login { get; set; }
 
+        [Required]
+        [MaxLength(16)]
         public string Password { get; set; }
 
         public ICollection<Cost> User_Costs { get; set; }
