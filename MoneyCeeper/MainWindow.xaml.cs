017541bea4c23a1d97fb7aaff0bddec0860bc902
DOJ745 <andreyfa12@mail.ru>
2020-05-09 15:19:28 +0300

    Работа будет через CodeFirst

---
+++ MoneyCeeper/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MoneyCeeper.Model;
+
+namespace MoneyCeeper
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            using (MainModel context = new MainModel())
+            {
+                //User temp = new User {Login = "Login", Password = "123456" };
+               // context.User.Add(temp);
+                //context.SaveChanges();
+                foreach(var elem in context.User)
+                {
+                    MessageBox.Show("Login - " + elem.Login + ": Password - " + elem.Password);
+                }
+            }
+        }
+    }
+}
---
+++ MoneyCeeper/Model/User.cs
@@ -0,0 +1,29 @@
+namespace MoneyCeeper.Model
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("User")]
+    public partial class User
+    {
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
+        public User()
+        {
+            Cost = new HashSet<Cost>();
+        }
+
+        [Key]
+        [StringLength(32)]
+        public string Login { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Password { get; set; }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
+        public virtual ICollection<Cost> Cost { get; set; }
+    }
+}
